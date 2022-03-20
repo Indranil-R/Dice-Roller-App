b514519898bb7004b0b4e922d818398b57f4ef32
Indranil Roy <indranilroy2003@gmail.com>
2022-03-20 22:54:41 +0530

    Dice Roll

--- app/src/main/java/corp/thunder/diceroller/MainActivity.kt
+++ app/src/main/java/corp/thunder/diceroller/MainActivity.kt
@@ -2,7 +2,9 @@ package corp.thunder.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,20 +12,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button : Button =findViewById(R.id.button)
-        val textView: TextView=findViewById(R.id.textView)
+    }
 
-        button.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT)
-            val dice:Dice=Dice()
-            toast.show()
-            textView.text=dice.roll().toString()
 
+    fun rollDice(view: View) {
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val dice: Dice = Dice()
+
+        val imageSrc = when (dice.roll()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            2 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        diceImage.setImageResource(imageSrc)
+//        Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
+
     }
 }
-class Dice{
-    fun roll():Int{
+
+class Dice {
+    fun roll(): Int {
         return (1..6).random()
     }
 }
